9722d0bb36cbb54f7dc73736e16fedcb7ce0e06f
tsorrels <tylersorrels1@gmail.com>
2018-01-21 09:20:13 -0800

    built out variable_parser.py

--- variable_parser.py
+++ variable_parser.py
@@ -1,5 +1,64 @@
+delimeters = [
+    '.',
+    '=',
+    '+',
+    '/',
+    '%',
+    '>',
+    '<',
+    '>',
+    '&'
+
+    ]
 
 
+class Variable(object):
+    def __init__(self, symbol, fields = [], methods = []):
+        self.symbol = ''
+        self.fields = fields
+        self.methods = methods
+
+    def to_string(self):
+
+        # use string.format()
+        pass 
+
+
+def is_keyword(word):
+    return False
+
+def parse_line(line):
+    symbol = ''
+    index = 0
+    while index < len(line):        
+        if line[index] in delimeters:
+            break
+        else:
+            symbol += line[index]
+            index ++
+
+    variable = Variable(symbol)
+    return variable
+    
+
+def parse_input(input):
+    variables = []
+    index = 0
+    line = ''
+    while index < len(input):
+        if input[index] != '\n':
+            line += input[index]
+            
+        else:
+            variable = parse_line(line)
+            if variable:
+                variables.append(variable)
+            line = ''  #reset line variable
+    
+        index ++
+
+    return variables
+            
 def get_stdin():
     returned_text = ''
     while(True):
@@ -12,12 +71,14 @@ def get_stdin():
 
 
 
-
+def write_output(variables):
+    pass
 
 
 def main():
     input = get_stdin()
-    pass
+    variables = parse_input(input)
+    write_output(variables)
 
 
 if __name__ == '__main__':
