d0c06214837b89ea7e535c36fbb2761d34103893
tsorrels <tsorrels@github.com>
2018-01-30 19:14:46 -0800

    updating python parser to handle comments

--- variable_parser/variable_parser.py
+++ variable_parser/variable_parser.py
@@ -24,13 +24,25 @@ class VariableParser(object):
 
     def parse_line(self, line):
         index = 0
+        last_char_index = len(line) - 1
+
+        # check for a comment
+        try:
+            last_char_index = line.index('#')
+
+        except ValueError:
+            pass
+        # splice off comment
+        line = line[index:last_char_index]
+
         #strip whitespace
         while index < len(line):
             if line[index] in string.whitespace:
                 index += 1
             else:
                 break
-        
+
+        #build symbol
         symbol = ''
         while index < len(line):        
             if line[index] in delimeters:
@@ -38,13 +50,13 @@ class VariableParser(object):
             else:
                 symbol += line[index]
             index+=1
-
+            
         #symbol = symbol.strip() #remove white space
         if symbol != '' and not keyword.iskeyword(symbol):
             variable = Variable(symbol)
-
+            
         #check for variable assignment
-        if '=' in line[index:]:
+        if '=' in line[index:last_char_index]:
             return variable
         
         return None
