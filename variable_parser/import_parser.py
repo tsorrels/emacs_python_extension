dc214a811bad58f263a14f753d890bb015c8c38d
tsorrels <tsorrels@github.com>
2018-03-01 18:09:02 -0800

    adding test_package, nested python modules

--- variable_parser/import_parser.py
+++ variable_parser/import_parser.py
@@ -9,34 +9,11 @@ class ImportParser(object):
         self.packages = []
 
     def parse_package(self, package_name):
-        methods = []
-        members = []
-
-        #print 'importing ' + package_name
         
         try:
             importlib.import_module(package_name)
-
             module = sys.modules[package_name]
-            
-            for key, value in module.__dict__.iteritems():
-                if key[0] == '_': # if private member, like __copywrite__
-                    continue
-
-                #print key, value
-                
-                # TODO: parse classes
-
-                if (isinstance(value, types.FunctionType) or
-                    isinstance(value, types.BuiltinFunctionType) or
-                    isinstance(value, types.BuiltinMethodType)):
-                    methods.append(key)
-
-                else:
-                    members.append(key)
-
-            variable = Variable(package_name, members, methods)
-
+            variable = parse_complex_type(module)            
             return variable
                     
         except Exception as E:
@@ -44,3 +21,31 @@ class ImportParser(object):
             # no retry logic or special error handling
             # TODO: log something
             pass
+
+    def parse_complex_type(object):
+        methods = []
+        members = []
+        variables = []
+
+        for key, value in module.__dict__.iteritems():
+            if key[0] == '_': # if private member, like __copywrite__
+                continue
+
+            if (isinstance(value, types.FunctionType) or
+                isinstance(value, types.BuiltinFunctionType) or
+                isinstance(value, types.BuiltinMethodType)):
+                methods.append(key)
+
+            elif hasattr(value, '__dict__'):
+                # this is not a function, so must be complex variable type
+                complex_object = parse_complex_type(value)
+                if complex_object:
+                    variables.append(complex_object)
+                                        
+            else:
+                members.append(key)
+
+        variable = Variable(package_name, members, methods, variables)
+        
+        return variable
+    
--- variable_parser/variable.py
+++ variable_parser/variable.py
@@ -8,15 +8,22 @@ def format_list(list):
     return string
 
 class Variable(object):
-    def __init__(self, symbol, fields = [], methods = []):
+    def __init__(self, symbol, fields = [], methods = [], variables = []):
         self.symbol = symbol
         self.fields = fields
         self.methods = methods
+        self.variables = variables
 
-    def to_string(self):        
+    def to_string(self, write_variables = False):        
         fields_string = format_list(self.fields)
-        methods_string = format_list(self.methods)        
-        string = '{0};{1};{2}'.format(self.symbol,
-                                      fields_string,
-                                      methods_string) 
+        methods_string = format_list(self.methods)
+
+        if write_variables:
+            string = '{0};{1};{2};{3}'.format(self.symbol,
+                                              fields_string,
+                                              methods_string)
+        else:
+            string = '{0};{1};{2}'.format(self.symbol,
+                                              fields_string)
+            
         return string
